4aa70bf39ac0e90befc85ac836d453167f303f9d
Мухамеджанова Эльмира <timra.work@gmail.com>
2020-08-16 18:48:13 +0500

    Rewrite filters into classes
    - Add class to filter.js
    - Use method .getElement and pass the PARAMETERS

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import {TASK_COUNT, TASK_COUNT_PER_STEP} from "./const.js";
 import MenuView from "./view/menu.js";
+import FilterView from "./view/filter.js";
 import BoardView from "./view/board.js";
 import SortView from "./view/sort.js";
 import TaskListView from "./view/task-list.js";
 import LoadMoreView from "./view/load-more.js";
 
-import {createFilterTemplate} from "./view/filter.js";
 import {createTaskTemplate} from "./view/task.js";
 import {createTaskEditTemplate} from "./view/task-edit.js";
 
@@ -20,26 +20,21 @@ const tasks = new Array(TASK_COUNT).fill().map(generateTask);
 const filters = generateFilter(tasks);
 
 renderElement(header, new MenuView().getElement(), RenderPosition.BEFOREEND);
-renderTemplate(main, createFilterTemplate(filters), `beforeend`);
+// renderTemplate(main, createFilterTemplate(filters), `beforeend`);
+renderElement(main, new FilterView(filters).getElement(), RenderPosition.BEFOREEND);
 
 
 const boardComponent = new BoardView();
-// renderTemplate(main, createBoardTemplate(), `beforeend`);
 renderElement(main, boardComponent.getElement(), RenderPosition.BEFOREEND);
-// const board = main.querySelector(`.board`);
-// renderTemplate(board, createSortTemplate(), `afterbegin`);
 renderElement(boardComponent.getElement(), new SortView().getElement(), RenderPosition.AFTERBEGIN);
 
 
 const taskListComponent = new TaskListView();
 renderElement(boardComponent.getElement(), taskListComponent.getElement(), RenderPosition.BEFOREEND);
-// const taskList = board.querySelector(`.board__tasks`);
-// renderTemplate(taskList, createTaskEditTemplate(tasks[0]), `beforeend`);
 renderTemplate(taskListComponent.getElement(), createTaskEditTemplate(tasks[0]), RenderPosition.BEFOREEND);
 
 
 for (let i = 1; i < Math.min(tasks.length, TASK_COUNT_PER_STEP); i++) {
-  // renderTemplate(taskList, createTaskTemplate(tasks[i]), `beforeend`);
   renderTemplate(taskListComponent.getElement(), createTaskTemplate(tasks[i]), RenderPosition.BEFOREEND);
 }
 
@@ -47,12 +42,8 @@ if (tasks.length > TASK_COUNT_PER_STEP) {
   let renderedTaskCount = TASK_COUNT_PER_STEP;
 
   const loadMoreButtonComponent = new LoadMoreView();
-  // renderTemplate(board, createLoadMoreTemplate(), `beforeend`);
   renderElement(boardComponent.getElement(), loadMoreButtonComponent.getElement(), RenderPosition.BEFOREEND);
 
-
-  // const loadMoreButton = board.querySelector(`.load-more`);
-  // loadMoreButton.addEventListener(`click`, (evt) => {
   loadMoreButtonComponent.getElement().addEventListener(`click`, (evt) => {
     evt.preventDefault();
     tasks
@@ -62,7 +53,6 @@ if (tasks.length > TASK_COUNT_PER_STEP) {
     renderedTaskCount += TASK_COUNT_PER_STEP;
 
     if (renderedTaskCount >= tasks.length) {
-      // loadMoreButton.remove();
       loadMoreButtonComponent.getElement().remove();
       loadMoreButtonComponent.removeElement();
     }
