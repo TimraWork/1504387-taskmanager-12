8deaf229bdd92601a820dacd116a810fe3e02f3d
Мухамеджанова Эльмира <timra.work@gmail.com>
2020-08-21 18:56:02 +0500

    Add abstract class
    - Create file view -> abstract.js with abstract class
    - Extends simple components from new abstract class

--- src/view/load-more.js
+++ src/view/load-more.js
@@ -1,4 +1,4 @@
-import {createElement} from "../utils.js";
+import AbstractView from "./abstract.js";
 
 const createLoadMoreTemplate = () => {
   return (
@@ -6,24 +6,8 @@ const createLoadMoreTemplate = () => {
   );
 };
 
-export default class classLoadMore {
-  constructor() {
-    this._element = null;
-  }
-
+export default class classLoadMore extends AbstractView {
   getTemplate() {
     return createLoadMoreTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
