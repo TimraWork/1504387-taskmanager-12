e4febb932bb3687734b72c21f1138835132ab8fc
Мухамеджанова Эльмира <timra.work@gmail.com>
2020-08-22 18:26:23 +0500

    Set logic from main.js to presenter/board.js

--- src/main.js
+++ src/main.js
@@ -1,101 +1,21 @@
-import {TASK_COUNT, TASK_COUNT_PER_STEP} from "./const.js";
+import {TASK_COUNT} from "./const.js";
 import MenuView from "./view/menu.js";
 import FilterView from "./view/filter.js";
-import BoardView from "./view/board.js";
-import SortView from "./view/sort.js";
-import TaskListView from "./view/task-list.js";
-import TaskView from "./view/task.js";
-import TaskEditView from "./view/task-edit.js";
-import NoTaskView from "./view/no-task.js";
-import LoadMoreView from "./view/load-more.js";
 
 import {generateTask} from "./mock/task.js";
 import {generateFilter} from "./mock/filter.js";
-import {render, RenderPosition, replace, remove} from "./utils/render.js";
-
-const main = document.querySelector(`.main`);
-const header = main.querySelector(`.main__control`);
-
-const renderTask = (taskListElement, task) => {
-  const taskComponent = new TaskView(task);
-  const taskEditComponent = new TaskEditView(task);
-
-  const replaceCardToForm = () => {
-    replace(taskEditComponent, taskComponent);
-  };
-
-  const replaceFormToCard = () => {
-    replace(taskComponent, taskEditComponent);
-  };
-
-  const onEscKeyDown = (evt) => {
-    if (evt.key === `Escape` || evt.key === `Esc`) {
-      evt.preventDefault();
-      replaceFormToCard();
-      document.removeEventListener(`keydown`, onEscKeyDown);
-    }
-  };
-
-  taskComponent.setEditClickHandler(() => {
-    replaceCardToForm();
-    document.addEventListener(`keydown`, onEscKeyDown);
-  });
-
-  taskEditComponent.setFormSubmitHandler(() => {
-    replaceFormToCard();
-    document.removeEventListener(`keydown`, onEscKeyDown);
-  });
-
-  render(taskListElement, taskComponent.getElement(), RenderPosition.BEFOREEND);
-};
+import BoardPresenter from "./presenter/board.js";
+import {render, RenderPosition} from "./utils/render.js";
 
 const tasks = new Array(TASK_COUNT).fill().map(generateTask);
 const filters = generateFilter(tasks);
 
+const main = document.querySelector(`.main`);
+const header = main.querySelector(`.main__control`);
 
-const renderBoard = (boardContainer, boardTasks) => {
-
-  const boardComponent = new BoardView();
-  const taskListComponent = new TaskListView();
-
-  render(boardContainer, boardComponent, RenderPosition.BEFOREEND);
-
-  render(boardContainer, boardComponent, RenderPosition.BEFOREEND);
-
-  if (boardTasks.every((task) => task.isArchive)) {
-    render(boardComponent.getElement(), new NoTaskView(), RenderPosition.BEFOREEND);
-    return;
-  }
-  // render(boardComponent.getElement(), new SortView().getElement(), RenderPosition.BEFOREEND);
-  render(boardComponent, new SortView(), RenderPosition.AFTERBEGIN);
-  render(boardComponent, taskListComponent, RenderPosition.BEFOREEND);
-
-  boardTasks
-    .slice(0, Math.min(tasks.length, TASK_COUNT_PER_STEP))
-    .forEach((boardTask) => renderTask(taskListComponent.getElement(), boardTask));
-
-  if (boardTasks.length > TASK_COUNT_PER_STEP) {
-    let renderedTaskCount = TASK_COUNT_PER_STEP;
-
-    const loadMoreButtonComponent = new LoadMoreView();
-    render(boardComponent, loadMoreButtonComponent, RenderPosition.BEFOREEND);
-
-    loadMoreButtonComponent.setClickHandler(() => {
-      boardTasks
-        .slice(renderedTaskCount, renderedTaskCount + TASK_COUNT_PER_STEP)
-        .forEach((task) => renderTask(taskListComponent.getElement(), task));
-
-      renderedTaskCount += TASK_COUNT_PER_STEP;
-
-      if (renderedTaskCount >= boardTasks.length) {
-        remove(loadMoreButtonComponent);
-      }
-    });
-  }
-
-};
+const boardPresenter = new BoardPresenter(main);
 
 render(header, new MenuView(), RenderPosition.BEFOREEND);
 render(main, new FilterView(filters), RenderPosition.BEFOREEND);
 
-renderBoard(main, tasks);
+boardPresenter.init(tasks);
--- src/presenter/board.js
+++ src/presenter/board.js
@@ -4,8 +4,10 @@ import TaskListView from "../view/task-list.js";
 import NoTaskView from "../view/no-task.js";
 import TaskView from "../view/task.js";
 import TaskEditView from "../view/task-edit.js";
-import LoadMoreButtonView from "../view/load-more-button.js";
-import {render, RenderPosition} from "../utils/render.js";
+import LoadMoreButtonView from "../view/load-more.js";
+import {render, RenderPosition, replace, remove} from "../utils/render.js";
+
+const TASK_COUNT_PER_STEP = 8;
 
 export default class Board {
   constructor(boardContainer) {
@@ -19,34 +21,95 @@ export default class Board {
 
   init(boardTasks) {
     this._boardTasks = boardTasks.slice();
-    // Метод для инициализации (начала работы) модуля,
-    // малая часть текущей функции renderBoard в main.js
+
+    render(this._boardContainer, this._boardComponent, RenderPosition.BEFOREEND);
+    render(this._boardComponent, this._taskListComponent, RenderPosition.BEFOREEND);
+
+    this._renderBoard();
   }
 
   _renderSort() {
-    // Метод для рендеринга сортировки
+    render(this._boardComponent, this._sortComponent, RenderPosition.AFTERBEGIN);
   }
 
-  _renderTask() {
-    // Метод, куда уйдёт логика созданию и рендерингу компонетов задачи,
-    // текущая функция renderTask в main.js
+  _renderTask(task) {
+    const taskComponent = new TaskView(task);
+    const taskEditComponent = new TaskEditView(task);
+
+    const replaceCardToForm = () => {
+      replace(taskEditComponent, taskComponent);
+    };
+
+    const replaceFormToCard = () => {
+      replace(taskComponent, taskEditComponent);
+    };
+
+    const onEscKeyDown = (evt) => {
+      if (evt.key === `Escape` || evt.key === `Esc`) {
+        evt.preventDefault();
+        replaceFormToCard();
+        document.removeEventListener(`keydown`, onEscKeyDown);
+      }
+    };
+
+    taskComponent.setEditClickHandler(() => {
+      replaceCardToForm();
+      document.addEventListener(`keydown`, onEscKeyDown);
+    });
+
+    taskEditComponent.setFormSubmitHandler(() => {
+      replaceFormToCard();
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    });
+
+    render(this._taskListComponent, taskComponent, RenderPosition.BEFOREEND);
   }
 
-  _renderTasks() {
-    // Метод для рендеринга N-задач за раз
+  _renderTasks(from, to) {
+    this._boardTasks
+      .slice(from, to)
+      .forEach((boardTask) => this._renderTask(boardTask));
   }
 
   _renderNoTasks() {
-    // Метод для рендеринга заглушки
+    render(this._boardComponent, this._noTaskComponent, RenderPosition.AFTERBEGIN);
   }
 
   _renderLoadMoreButton() {
-    // Метод, куда уйдёт логика по отрисовке компонетов задачи,
-    // текущая функция renderTask в main.js
+    let renderedTaskCount = TASK_COUNT_PER_STEP;
+
+    const loadMoreButtonComponent = new LoadMoreButtonView();
+
+    render(this._boardComponent, loadMoreButtonComponent, RenderPosition.BEFOREEND);
+
+    loadMoreButtonComponent.setClickHandler(() => {
+      this._boardTasks
+        .slice(renderedTaskCount, renderedTaskCount + TASK_COUNT_PER_STEP)
+        .forEach((boardTask) => this._renderTask(boardTask));
+
+      renderedTaskCount += TASK_COUNT_PER_STEP;
+
+      if (renderedTaskCount >= this._boardTasks.length) {
+        remove(loadMoreButtonComponent);
+      }
+    });
+  }
+
+  _renderTaskList() {
+    this._renderTasks(0, Math.min(this._boardTasks.length, TASK_COUNT_PER_STEP));
+
+    if (this._boardTasks.length > TASK_COUNT_PER_STEP) {
+      this._renderLoadMoreButton();
+    }
   }
 
   _renderBoard() {
-    // Метод для инициализации (начала работы) модуля,
-    // бОльшая часть текущей функции renderBoard в main.js
+    if (this._boardTasks.every((task) => task.isArchive)) {
+      this._renderNoTasks();
+      return;
+    }
+
+    this._renderSort();
+    this._renderTaskList();
   }
 }
