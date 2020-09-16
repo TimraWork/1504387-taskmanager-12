86369d9f3009821652d4736a10b1b1f952dcd828
Мухамеджанова Эльмира <timra.work@gmail.com>
2020-09-16 19:08:40 +0500

    7.9 Add filter presenter

--- src/main.js
+++ src/main.js
@@ -1,23 +1,15 @@
 import {TASK_COUNT} from "./const.js";
 import MenuView from "./view/menu.js";
-import FilterView from "./view/filter.js";
 
 import {generateTask} from "./mock/task.js";
 import {render, RenderPosition} from "./utils/render.js";
 
 import BoardPresenter from "./presenter/board.js";
+import FilterPresenter from "./presenter/filter.js";
 import TasksModel from "./model/tasks.js";
 import FilterModel from "./model/filter.js";
 
 const tasks = new Array(TASK_COUNT).fill().map(generateTask);
-// const filters = generateFilter(tasks);
-const filters = [
-  {
-    type: `all`,
-    name: `ALL`,
-    count: 0
-  }
-];
 
 const tasksModel = new TasksModel();
 tasksModel.setTasks(tasks);
@@ -28,8 +20,9 @@ const main = document.querySelector(`.main`);
 const header = main.querySelector(`.main__control`);
 
 const boardPresenter = new BoardPresenter(main, tasksModel);
+const filterPresenter = new FilterPresenter(main, filterModel, tasksModel);
 
 render(header, new MenuView(), RenderPosition.BEFOREEND);
-render(main, new FilterView(filters, `all`), RenderPosition.BEFOREEND);
 
+filterPresenter.init();
 boardPresenter.init();
