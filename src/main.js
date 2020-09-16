52158ac2a192d958ed19db40aef167bbe55c64af
Мухамеджанова Эльмира <timra.work@gmail.com>
2020-09-16 18:53:05 +0500

    7.8 Refactor filter

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import MenuView from "./view/menu.js";
 import FilterView from "./view/filter.js";
 
 import {generateTask} from "./mock/task.js";
-import {generateFilter} from "./mock/filter.js";
 import {render, RenderPosition} from "./utils/render.js";
 
 import BoardPresenter from "./presenter/board.js";
@@ -11,7 +10,14 @@ import TasksModel from "./model/tasks.js";
 import FilterModel from "./model/filter.js";
 
 const tasks = new Array(TASK_COUNT).fill().map(generateTask);
-const filters = generateFilter(tasks);
+// const filters = generateFilter(tasks);
+const filters = [
+  {
+    type: `all`,
+    name: `ALL`,
+    count: 0
+  }
+];
 
 const tasksModel = new TasksModel();
 tasksModel.setTasks(tasks);
@@ -24,6 +30,6 @@ const header = main.querySelector(`.main__control`);
 const boardPresenter = new BoardPresenter(main, tasksModel);
 
 render(header, new MenuView(), RenderPosition.BEFOREEND);
-render(main, new FilterView(filters), RenderPosition.BEFOREEND);
+render(main, new FilterView(filters, `all`), RenderPosition.BEFOREEND);
 
 boardPresenter.init();
